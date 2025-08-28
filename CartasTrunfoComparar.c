554fe4e79c27dde381129432cea89c001e154979
Daniel Pinto <164277441+Danfreitasp@users.noreply.github.com>
2025-08-28 20:40:37 -0300

    Update CartasTrunfoComparar.c
    Adicionado o pib per capita e conversão para bilhões.

--- CartasTrunfoComparar.c
+++ CartasTrunfoComparar.c
@@ -5,7 +5,7 @@ int main() {
     char estado2[15] = "Espirito Santo";
     char codigo1[5] = "AB01", codigo2[5] = "CD02", cidade1[15] = "Belo Horizonte", cidade2[15] = "Vitoria";
     int populacao1 = 2315560, populacao2 = 322869, turismo1 = 40, turismo2 = 25;
-    float area1 = 331.4, area2 = 93.38, pib1 = 105.3, pib2 = 31.42, densidade1, densidade2;
+    float area1 = 331.4, area2 = 93.38, pib1 = 105.3, pib2 = 31.42, densidade1, densidade2, pibpercapita1, pibpercapita2;
 
     
     // Exibição
@@ -19,6 +19,8 @@ int main() {
     printf("Pontos Turísticos: %d\n", turismo1);
     densidade1 = populacao1 / area1;
     printf("Densidade Demográfica: %.2f habitantes/km²\n", densidade1);
+    pibpercapita1 = (pib1 * 1e9) / populacao1; // 1e9 = Convertendo PIB de bilhões para reais
+    printf("PIB per capita: %.2f reais\n", pibpercapita1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %s\n", estado2);
@@ -30,6 +32,8 @@ int main() {
     printf("Pontos Turísticos: %d\n", turismo2);
     densidade2 = populacao2 / area2;
     printf("Densidade Demográfica: %.2f habitantes/km²\n", densidade2);
+    pibpercapita2 = (pib2 * 1e9) / populacao2; // 1e9 = Convertendo PIB de bilhões para reais
+    printf("PIB per capita: %.2f reais\n", pibpercapita2);
 
     if (pib1 > pib2) {
         printf("\nA carta 1 tem o maior PIB.\n");
